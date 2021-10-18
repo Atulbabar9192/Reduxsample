44c405e51a0902ce9ba96c92f93766576c6820a3
Atulbabar9192 <Atulbabar9192@users.noreply.github.com>
2021-10-18 21:58:21 +0530

    Update TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,29 +1,32 @@
-import React, { Component } from 'react'
-import {connect } from 'react-redux'
-import {ToggleTodo} from '../action'
-class TodoList extends Component{
-    render(){
-        console.log(this.props.todos)
-        return(
-            <div>
-            <ul>
-                {this.props.todos.map(item=>(
-                    <div key={item.id}>
-                     <li >{item.text}</li>
-                     {!item.completed? <button onClick={()=>this.props.ToggleTodo(item.id)}>Complete</button>:null}
-                    </div>
-                ))}
-            </ul>
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { ToggleTodo } from "../action";
+class TodoList extends Component {
+  render() {
+    console.log(this.props.todos);
+    return (
+      <div>
+        <ul>
+          {this.props.todos.map((item) => (
+            <div key={item.id}>
+              <li>{item.text}</li>
+              {!item.completed ? (
+                <button onClick={() => this.props.ToggleTodo(item.id)}>
+                  Complete
+                </button>
+              ) : null}
             </div>
-                
-        )
-    }
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps=(state)=>{
-    return{
-        todos:state.todos
-    }
-}
+const mapStateToProps = (state) => {
+  return {
+    todos: state.todos,
+  };
+};
 
-export default connect(mapStateToProps,{ToggleTodo})(TodoList)
+export default connect(mapStateToProps, { ToggleTodo })(TodoList);
